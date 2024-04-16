d9fd825ddab9a1efcf714f5c0fbf4288574fbac4
amuwall <amuwall@outlook.com>
2024-04-16 20:22:35 +0800

    feat: add request and params

---
+++ params.go
@@ -0,0 +1,124 @@
+package request
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/url"
+)
+
+type QueryParams map[string][]string
+
+func NewQueryParams(params map[string]string) QueryParams {
+	p := QueryParams{}
+	for key, value := range params {
+		p.Set(key, value)
+	}
+	return p
+}
+
+func (p QueryParams) Set(key, value string) {
+	p[key] = []string{value}
+}
+
+func (p QueryParams) Add(key, value string) {
+	p[key] = append(p[key], value)
+}
+
+func (p QueryParams) Get(key string) string {
+	value := p[key]
+	if len(value) == 0 {
+		return ""
+	}
+	return value[0]
+}
+
+func (p QueryParams) Encode() string {
+	return url.Values(p).Encode()
+}
+
+type BodyParams interface {
+	Build() (contentType string, body io.Reader, err error)
+}
+
+type JsonBodyParams struct {
+	params interface{}
+}
+
+func NewJsonBodyParams(params interface{}) *JsonBodyParams {
+	return &JsonBodyParams{
+		params: params,
+	}
+}
+
+func (p *JsonBodyParams) Build() (contentType string, body io.Reader, err error) {
+	data, err := json.Marshal(p.params)
+	if err != nil {
+		err = fmt.Errorf("marshal error %w", err)
+		return
+	}
+
+	contentType = "application/json; charset=utf-8"
+	body = bytes.NewReader(data)
+
+	return
+}
+
+type FormBodyParams struct {
+	params map[string]string
+	files  []*formFile
+}
+
+type formFile struct {
+	FieldName string
+	FileName  string
+	Reader    io.Reader
+}
+
+func NewFormBodyParams(params map[string]string) *FormBodyParams {
+	return &FormBodyParams{
+		params: params,
+		files:  nil,
+	}
+}
+
+func (p *FormBodyParams) AddFile(fieldName, fileName string, reader io.Reader) {
+	p.files = append(p.files, &formFile{
+		FieldName: fieldName,
+		FileName:  fileName,
+		Reader:    reader,
+	})
+}
+
+func (p *FormBodyParams) Build() (contentType string, body io.Reader, err error) {
+	buffer := &bytes.Buffer{}
+	body = buffer
+
+	bodyWriter := multipart.NewWriter(buffer)
+	defer bodyWriter.Close()
+
+	for key, value := range p.params {
+		err = bodyWriter.WriteField(key, value)
+		if err != nil {
+			err = fmt.Errorf("write field %s error %w", key, err)
+			return
+		}
+	}
+
+	for _, file := range p.files {
+		fileWriter, err := bodyWriter.CreateFormFile(file.FieldName, file.FileName)
+		if err != nil {
+			return "", nil, err
+		}
+		_, err = io.Copy(fileWriter, file.Reader)
+		if err != nil {
+			return "", nil, err
+		}
+	}
+
+	contentType = bodyWriter.FormDataContentType()
+
+	return
+}
--- request.go
+++ request.go
@@ -1,9 +1,113 @@
 package request
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
 type Request struct {
+	Method string
+	Path   string
+
+	Host    string
+	Headers http.Header
+
+	QueryParams QueryParams
+	BodyParams  BodyParams
 }
 
-func NewRequest() *Request {
-	// TODO:
-	return nil
+type RequestOption func(*Request) error
+
+func NewRequest(method, path string, options ...RequestOption) (request *Request, err error) {
+	request = &Request{
+		Method: method,
+		Path:   path,
+
+		Host:    "",
+		Headers: http.Header{},
+	}
+
+	for _, option := range options {
+		err = option(request)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func WithHost(host string) RequestOption {
+	return func(r *Request) error {
+		r.Host = host
+		return nil
+	}
+}
+
+func WithHeaders(headers map[string]string) RequestOption {
+	return func(r *Request) error {
+		for k, v := range headers {
+			r.Headers.Set(k, v)
+		}
+		return nil
+	}
+}
+
+func WithQueryParams(queryParams QueryParams) RequestOption {
+	return func(r *Request) error {
+		r.QueryParams = queryParams
+		return nil
+	}
+}
+
+func WithBodyParams(bodyParams BodyParams) RequestOption {
+	return func(r *Request) error {
+		r.BodyParams = bodyParams
+		return nil
+	}
+}
+
+func (req *Request) build(baseURL string) (httpRequest *http.Request, err error) {
+	requestURL, err := url.JoinPath(baseURL, req.Path)
+	if err != nil {
+		err = fmt.Errorf("build url path error %w", err)
+		return
+	}
+
+	var requestBody io.Reader
+	if req.BodyParams != nil {
+		var contentType string
+		contentType, requestBody, err = req.BodyParams.Build()
+		if err != nil {
+			err = fmt.Errorf("build body params error %w", err)
+			return
+		}
+		if contentType != "" {
+			if req.Headers.Get("Content-Type") != "" {
+				req.Headers.Set("Content-Type", contentType)
+			}
+		}
+	}
+
+	httpRequest, err = http.NewRequest(req.Method, requestURL, requestBody)
+	if err != nil {
+		err = fmt.Errorf("new http request error %w", err)
+		return
+	}
+
+	if len(req.Host) != 0 {
+		httpRequest.Host = req.Host
+	}
+
+	if req.Headers != nil {
+		httpRequest.Header = req.Headers
+	}
+
+	if len(req.QueryParams) != 0 {
+		httpRequest.URL.RawQuery = req.QueryParams.Encode()
+	}
+
+	return
 }
